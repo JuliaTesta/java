26919799f42e3d2a8be842316a0365cdffcd8817
JuliaTesta <jutemts@gmail.com>
2025-06-06 16:12:19 -0300

    atualizado

---
+++ Exercicio004/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.CurrencyConverter;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.printf("What is the dollar price? ");
+		double dollarPrice = sc.nextDouble();	
+		
+		System.out.printf("How many dollars will be bought? ");
+		double amount = sc.nextDouble();
+		
+		double result = CurrencyConverter.calculator(dollarPrice, amount);
+		System.out.printf("Amount to be paid in reais = %.2f", result);
+		sc.close();
+	}
+}
